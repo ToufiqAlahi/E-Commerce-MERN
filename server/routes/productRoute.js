0d70ca89bd3ad5cd33d6eb2c24c65b4a88bee3ce
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-04 12:02:13 +0600

    basic server is running

---
+++ server/routes/productRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { route } = require("../app");
+const { getAllProducts } = require("../controllers/productController");
+
+const router = express.Router();
+
+router.route("/products").get(getAllProducts);
+
+module.exports = router;
