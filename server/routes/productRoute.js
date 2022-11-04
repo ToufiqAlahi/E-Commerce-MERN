337228e5809c3c83ada63198f93140271479f9a2
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-04 14:29:16 +0600

    added update route

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -3,11 +3,13 @@ const route = require("../app");
 const {
   getAllProducts,
   createProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
