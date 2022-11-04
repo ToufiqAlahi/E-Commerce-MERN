1e3d9bdade66a5a0353691d62c271e76ffafac00
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-04 14:41:02 +0600

    added remove product api

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -19,6 +19,9 @@ exports.getAllProducts = async (req, res) => {
   });
 };
 
+
+
+
 // Update a specific product -- ADMIN
 exports.updateProduct = async (req, res, next) => {
   let product = await ProductData.findById(req.params.id);
@@ -39,3 +42,23 @@ exports.updateProduct = async (req, res, next) => {
     product,
   });
 };
+
+
+// Remove a specific product -- ADMIN
+
+exports.removeProduct = async (req, res, next) => {
+    const product = await ProductData.findById(req.params.id);
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product Not found",
+        });
+    }
+
+    await product.remove();
+    res.status(200).json({
+      success: true,
+      message: "Product Removed Successfully",
+    });
+
+}
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -4,12 +4,13 @@ const {
   getAllProducts,
   createProduct,
   updateProduct,
+  removeProduct,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
-router.route("/product/:id").put(updateProduct);
+router.route("/product/:id").put(updateProduct).delete(removeProduct);
 
 module.exports = router;
