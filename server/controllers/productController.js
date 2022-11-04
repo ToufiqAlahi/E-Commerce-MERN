d0134dd0177dcebad9e3b956c6ac62ad47884050
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-04 13:56:53 +0600

    API to create a new product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,3 +1,23 @@
+const Product = require("../models/productsModel");
+
+
+
+// Create Product 
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+
+
+
+
+
+
+
 exports.getAllProducts = (req, res) => {
     res.status(200).json({message:"Route is working just fine"})
 }
