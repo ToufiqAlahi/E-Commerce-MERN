e342c963be3f3091ea936a6d0a8495800113d1b2
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-04 14:04:36 +0600

    API to GET All product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,23 +1,20 @@
-const Product = require("../models/productsModel");
+const ProductData = require("../models/productsModel");
 
-
-
-// Create Product 
+// Create Product -- ADMIN
 exports.createProduct = async (req, res, next) => {
-    const product = await Product.create(req.body);
-    res.status(201).json({
-        success: true,
-        product
-    })
-}
+  const product = await ProductData.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
 
+// Get All Product
 
-
-
-
-
-
-
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({message:"Route is working just fine"})
-}
+exports.getAllProducts = async (req, res) => {
+  const products = await ProductData.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
