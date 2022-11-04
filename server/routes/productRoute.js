d72d17f2fe08d1b1bf319e5cab7404e151c07577
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-04 13:55:34 +0600

    added route to create a new product

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { route } = require("../app");
-const { getAllProducts } = require("../controllers/productController");
+const route = require("../app");
+const {
+  getAllProducts,
+  createProduct,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
 
 module.exports = router;
