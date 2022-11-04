be19bc2b830b54cdf44b331808fd7043adb962d7
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-04 12:33:27 +0600

    Connected MongoDB with Server
    Caught an error , server crashed!!! The useCreateIndex option has been deprecated for a while and removed as of the Mongoose 6 release per No More Deprecation Warning Options
    
    So, The solution is to remove any unsupported options from the code.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDatabase =require("./config/database")
+
 
 // config
+dotenv.config({ path: "server/config/.env" })
 
-dotenv.config({path:"server/config/.env"})
+// connecting to DB
+connectDatabase();
 
 
 const PORT = process.env.port;
