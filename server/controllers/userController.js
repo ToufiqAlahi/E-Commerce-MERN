ab3b24aa90f5e06169c108d2db4b2c5b5a8bfd98
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-06 21:24:38 +0600

    api for user Registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,20 @@
 const ErrorHandler = require("../Utils/errorHandler");
 const catchAsyncError = require("../Middleware/catchAsyncError");
-const user = require("../models/userModels");
+const User = require("../models/userModels");
 
 
 // User Registration:
 exports.createUser = catchAsyncError(async (req, res, next) => {
-    
+    const { name, email, password } = req.body;
+    const user = await User.create({
+        name, email, password,
+        avatar: {
+            public_Id: "sample ID",
+            url: "Profile pic url ",
+        }
+    });
+    res.status(201).json({
+        success: true,
+        user,
+    })
 })
