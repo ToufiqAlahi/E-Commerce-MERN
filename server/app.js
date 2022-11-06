0955dd6cd76bef1683fd29bc533ef5bd25adcc75
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-06 22:05:44 +0600

    added user route

--- server/app.js
+++ server/app.js
@@ -3,10 +3,13 @@ const app = express();
 const errorMiddleware = require("./Middleware/error");
 app.use(express.json());
 
-// ROute Imports
+
+// Route Imports
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
 
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 // Middleware for Error
 app.use(errorMiddleware);
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,2 +1,8 @@
 const express = require("express");
+const { createUser } = require("../controllers/userController");
 const router = express.Router();
+
+
+router.route("/register").post(createUser);
+
+module.exports = router;
