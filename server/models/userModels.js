d1202d665fc1803b0b8fd2b8813283e44a6e018c
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-08 20:01:08 +0600

    Hashed Reset token with sha256

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -66,6 +67,20 @@ userSchema.methods.getJWTToken = function () {
 
 userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function () {
+  //Generate Token:
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hashing and add to userSchema 
+  this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 100; // expires in 15 mins
+
+  return resetToken;
+
 };
 
 module.exports = mongoose.model("User", userSchema);
