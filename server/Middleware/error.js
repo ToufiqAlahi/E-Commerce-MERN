6f5b0ed21fe57d08de094b425bd212397a66dbcc
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-08 20:30:05 +0600

    handled Duplicate credentials error

--- server/Middleware/error.js
+++ server/Middleware/error.js
@@ -4,13 +4,18 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
-    // wrong mongoDB cast Error
-    if (err.name==="CastError") {
-        const message = `Resource not found. Invalid ${err.path} `;
-        err = new ErrorHandler(message,400)
-    }
-    
-    
+  // wrong mongoDB cast Error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid ${err.path} `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Mongoose Duplicate Key (E11000 duplicate key error) Error:
+  if (err.code === 11000) {
+     const message = `Duplicate ${Object.keys(err.keyValue)} is not acceptable`;
+     err = new ErrorHandler(message, 400);
+   }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
