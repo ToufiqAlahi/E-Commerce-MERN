cf567e85073cb158976c1919305d32b790b9728f
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-10 14:03:12 +0600

    Get Single Order info

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -3,35 +3,50 @@ const Product = require("../models/productsModel");
 const ErrorHandler = require("../Utils/errorHandler");
 const catchAsyncErrors = require("../Middleware/catchAsyncError");
 
-
-
 // Create new Order
 exports.newOrder = catchAsyncErrors(async (req, res, next) => {
-     const {
-       shippingInfo,
-       orderItems,
-       paymentInfo,
-       itemsPrice,
-       taxPrice,
-       shippingPrice,
-       totalPrice,
-     } = req.body;
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
 
-    
-      const order = await Order.create({
-        shippingInfo,
-        orderItems,
-        paymentInfo,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-        paidAt: Date.now(),
-        user: req.user._id,
-      });
-    
-     res.status(201).json({
-       success: true,
-       order,
-     });
-})
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user._id,
+  });
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+
+
+// get Single Order info: 
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
