e2e02d663dfe8552395b8dc89fe58c1b0e19e4d8
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-10 15:00:39 +0600

    update and delete order  --Admin

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -3,6 +3,9 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
 const router = express.Router();
 
@@ -10,10 +13,17 @@ const { isAuthenticatedUser, authorizedRoles } = require("../Middleware/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
-router
-  .route("/order/:id")
-  .get(isAuthenticatedUser, getSingleOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/admin/orders")
+    .get(isAuthenticatedUser, authorizedRoles("admin"), getAllOrders);
+  
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizedRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizedRoles("admin"), deleteOrder );
 
 module.exports = router;
