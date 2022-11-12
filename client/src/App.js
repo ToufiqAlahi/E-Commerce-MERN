68d4f39e2e9834cc815026f02e041f94403f0f36
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-12 21:48:54 +0600

    used WebFontLoader

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
-import Header from "./component/layout/Header.js";
+import Header from "./component/layout/Header/Header.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import WebFont from "webfontloader";
+import { useEffect } from "react";
 
 function App() {
 
+   useEffect(() => {
+     WebFont.load({
+       google: {
+         families: ["Roboto", "Droid Sans", "Chilanka"],
+       },
+     });
 
-  
+     
+   }, []);
+
   return (
     <Router>
       <Header />
