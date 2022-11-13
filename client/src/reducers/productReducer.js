3e8b904b5ca1c3039c097d3300a30025d7debf7d
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-13 13:19:52 +0600

    added productReducer

---
+++ client/src/reducers/productReducer.js
@@ -0,0 +1,31 @@
+import {
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCCESS,
+    ALL_PRODUCT_FAIL,
+} from "../constants/productConstants.js";
+
+export const productsReducer = (state = { products: [] }, action) => {
+    switch (action.type) {
+        case ALL_PRODUCT_REQUEST:
+            return {
+                loading: true,
+                products: [],
+            };
+        case ALL_PRODUCT_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload.products,
+                productsCount: action.payload.productsCount,
+            };
+ 
+        case ALL_PRODUCT_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+
+  
+    }
+};
