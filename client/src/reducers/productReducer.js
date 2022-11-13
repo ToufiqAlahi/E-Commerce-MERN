4bc0fd49571a98de586ffaaeb754ca0b61d81020
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-13 13:22:20 +0600

    clear error case

--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -2,30 +2,34 @@ import {
     ALL_PRODUCT_REQUEST,
     ALL_PRODUCT_SUCCESS,
     ALL_PRODUCT_FAIL,
+    CLEAR_ERRORS,
 } from "../constants/productConstants.js";
 
 export const productsReducer = (state = { products: [] }, action) => {
     switch (action.type) {
-        case ALL_PRODUCT_REQUEST:
-            return {
-                loading: true,
-                products: [],
-            };
-        case ALL_PRODUCT_SUCCESS:
-            return {
-                loading: false,
-                products: action.payload.products,
-                productsCount: action.payload.productsCount,
-            };
- 
-        case ALL_PRODUCT_FAIL:
-            return {
-                loading: false,
-                error: action.payload,
-            };
-        default:
-            return state;
+      case ALL_PRODUCT_REQUEST:
+        return {
+          loading: true,
+          products: [],
+        };
+      case ALL_PRODUCT_SUCCESS:
+        return {
+          loading: false,
+          products: action.payload.products,
+          productsCount: action.payload.productsCount,
+        };
 
-  
+      case ALL_PRODUCT_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        };
+      case CLEAR_ERRORS:
+        return {
+          ...state,
+          error: null,
+        };
+      default:
+        return state;
     }
 };
