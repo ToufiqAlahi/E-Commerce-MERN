6b71ea58a80ea4c427d99f7f1ea35afd9fa39b2b
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-13 12:16:30 +0600

    Update ProductCard.js

--- client/src/component/Home/ProductCard.js
+++ client/src/component/Home/ProductCard.js
@@ -3,27 +3,22 @@ import { Link } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 
 const ProductsCard = ({ products }) => {
-
   const options = {
-    
-      edit: "false",
-      color: "rgba(20,20,20,0.1)",
-      activeColor:"tomato",
-      value:3,
-      
-    };
-    
+    edit: "false",
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "tomato",
+    value: 3.5,
+    isHalf: true,
+  };
+
   return (
     <Link className="productsCard" to={products._id}>
       <img src={products.images[0].url} alt={products.name} />
-          <p>{products.name}</p>
-          
+      <p>{products.name}</p>
 
       <div>
         <ReactStars {...options} />
-        <span className="productsCardSpan">
-          (188 Reviews)
-        </span>
+        <span className="productsCardSpan">(188 Reviews)</span>
       </div>
       <span>{products.price}</span>
     </Link>
