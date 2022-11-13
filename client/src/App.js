87be75521adf4aa95e92983eb7bea3fd10b66f3d
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-13 10:00:25 +0600

    Added Route for Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Header from "./component/layout/Header/Header.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import WebFont from "webfontloader";
 import { useEffect } from "react";
 import Footer from "./component/layout/Footer/Footer";
-
+import Home from "./component/Home/Home.js";
 function App() {
 
    useEffect(() => {
@@ -20,6 +20,9 @@ function App() {
   return (
     <Router>
       <Header />
+
+      <Route exact path="/" component={Home} />
+
       <Footer></Footer>
     </Router>
   );
