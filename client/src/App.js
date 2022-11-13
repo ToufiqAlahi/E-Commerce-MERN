b9cdeaa5d1dad54ff7444729100cf709891e09c4
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-14 04:47:03 +0600

    migrated react-router-dom version

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./component/layout/Header/Header.js";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebFont from "webfontloader";
 import { useEffect } from "react";
 import Footer from "./component/layout/Footer/Footer";
@@ -18,12 +18,8 @@ function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/product/:id" element={<ProductDetails />} />
-        
-      </Routes>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/product/:id" component={ProductDetails} />
 
       <Footer />
     </Router>
