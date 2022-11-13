de0618acc38cc03459b1d95ed507a2c04f5b59d6
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-13 12:14:59 +0600

    Create ProductCard.js

---
+++ client/src/component/Home/ProductCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+
+const ProductsCard = ({ products }) => {
+
+  const options = {
+    
+      edit: "false",
+      color: "rgba(20,20,20,0.1)",
+      activeColor:"tomato",
+      value:3,
+      
+    };
+    
+  return (
+    <Link className="productsCard" to={products._id}>
+      <img src={products.images[0].url} alt={products.name} />
+          <p>{products.name}</p>
+          
+
+      <div>
+        <ReactStars {...options} />
+        <span className="productsCardSpan">
+          (188 Reviews)
+        </span>
+      </div>
+      <span>{products.price}</span>
+    </Link>
+  );
+};
+
+export default ProductsCard;
