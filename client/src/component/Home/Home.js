a27145d07a9177d12389defc5de40e39c9062198
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-13 12:53:24 +0600

    Added Helmet to change page tile Dynamically

--- client/src/component/Home/Home.js
+++ client/src/component/Home/Home.js
@@ -1,26 +1,25 @@
 import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/all";
+import MetaData from "../layout/MetaData";
 import "./Home.css";
 import ProductCard from "./ProductCard.js";
 
-
-
 const Home = () => {
-
-    const products = {
-      name: "Green Suit",
-      images: [
-        {
-          url: "https://images.unsplash.com/photo-1593032465175-481ac7f401a0?&w=220&q=50",
-        },
-      ],
-      price: "$7000",
-      _id: "Toufiq",
-    };
-
+  const products = {
+    name: "Green Suit",
+    images: [
+      {
+        url: "https://images.unsplash.com/photo-1593032465175-481ac7f401a0?&w=220&q=50",
+      },
+    ],
+    price: "$7000",
+    _id: "Toufiq",
+  };
 
   return (
     <Fragment>
+      <MetaData title="Home page is working" />
+
       <div className="banner">
         <p>Welcome to Shopzo</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
@@ -35,20 +34,17 @@ const Home = () => {
       <h2 className="homeHeading">Featured Products</h2>
 
       <div className="container" id="container">
-        
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-            <ProductCard products={products} />
-       
-          </div>
-          
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+        <ProductCard products={products} />
+      </div>
     </Fragment>
   );
 };
