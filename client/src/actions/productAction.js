1988fffbe271e48967bcea93e93d96b92f65adf6
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-15 12:10:28 +0600

    Update productAction.js

--- client/src/actions/productAction.js
+++ client/src/actions/productAction.js
@@ -11,27 +11,26 @@ import {
 } from "../constants/productConstants.js";
 
 // Get All Products
-export const getProduct = () => async (dispatch) => {
-  try {
-    dispatch({ type: ALL_PRODUCT_REQUEST });
+export const getProduct =
+  (keyword = "") =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: ALL_PRODUCT_REQUEST });
 
-    let link = `/api/v1/products`;
-    const { data } = await axios.get(link);
+      let link = `/api/v1/products?keyword${keyword}`;
+      const { data } = await axios.get(link);
 
-    dispatch({
-      type: ALL_PRODUCT_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: ALL_PRODUCT_FAIL,
-      payload: error.response.data.message,
-    });
-  }
-};
-
-
-
+      dispatch({
+        type: ALL_PRODUCT_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ALL_PRODUCT_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
 
 // Get Products Details
 export const getProductDetails = (id) => async (dispatch) => {
@@ -51,8 +50,6 @@ export const getProductDetails = (id) => async (dispatch) => {
     });
   }
 };
-
-
 
 // Clearing Errors
 export const clearErrors = () => async (dispatch) => {
