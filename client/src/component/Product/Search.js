f03233877c0729293b1605a0f9660ccdb80390ee
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-15 12:11:56 +0600

    search option

--- client/src/component/Product/Search.js
+++ client/src/component/Product/Search.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, Fragment } from "react";
+import MetaData from "../layout/MetaData";
+import "./Search.css";
 
-const Search = () => {
+const Search = ({ history }) => {
+  const [keyword, setKeyword] = useState("");
+
+  const searchSubmitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      history.push(`/products/${keyword}`);
+    } else {
+      history.push("/products");
+    }
+  };
+
   return (
-    <div>Search</div>
-  )
-}
+    <Fragment>
+      <MetaData title="Search A Product -- SHOPZO" />
+      <form className="searchBox" onSubmit={searchSubmitHandler}>
+        <input
+          type="text"
+          placeholder="Search a Product ..."
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </Fragment>
+  );
+};
 
-export default Search
+export default Search;
