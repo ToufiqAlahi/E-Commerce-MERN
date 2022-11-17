9e90f75312cbd39eb8050f2c710cba7f164ce144
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-17 21:18:31 +0600

    Update sendEmail.js

--- server/Utils/sendEmail.js
+++ server/Utils/sendEmail.js
@@ -1,10 +1,13 @@
-const nodemailer = require("nodemailer");
+const nodeMailer = require("nodemailer");
+
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
+  const transporter = nodeMailer.createTransport({
+    host: process.env.SMPT_HOST,
+    port: process.env.SMPT_PORT,
     service: process.env.SMPT_SERVICE,
     auth: {
       user: process.env.SMPT_MAIL,
-      pass : process.env.SMPT_PASSWORD,
+      pass: process.env.SMPT_PASSWORD,
     },
   });
 
