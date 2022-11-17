5d3d553599c8e192c90d1b1c589606577755fb53
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-17 21:09:58 +0600

    Update ProductCard.js

--- client/src/component/Home/ProductCard.js
+++ client/src/component/Home/ProductCard.js
@@ -1,33 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import ReactStars from "react-rating-stars-component";
 import { Rating } from "@material-ui/lab";
-import { productsReducer } from "../../reducers/productReducer";
 
-const ProductsCard = ({ product }) => {
+const ProductCard = ({ product }) => {
   const options = {
-    edit: "false",
-    color: "rgba(20,20,20,0.3)",
-    activeColor: "gold",
     value: product.ratings,
-    isHalf: true,
+    readOnly: true,
+    precision: 0.5,
   };
-  
   return (
     <Link className="productCard" to={`/product/${product._id}`}>
       <img src={product.images[0].url} alt={product.name} />
       <p>{product.name}</p>
-
       <div>
-        <Rating {...options} />
-        <span className="productsCardSpan">
+        <Rating {...options} />{" "}
+        <span className="productCardSpan">
           {" "}
           ({product.numOfReviews} Reviews)
         </span>
       </div>
-      <span>${product.price}</span>
+      <span>{`₹${product.price}`}</span>
     </Link>
   );
 };
 
-export default ProductsCard;
+export default ProductCard;
