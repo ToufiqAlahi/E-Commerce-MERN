49959e542d47d6105d7d0792653600d8c507d5e1
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-17 21:22:31 +0600

    Update auth.js

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -1,30 +1,33 @@
-const ErrorHandler = require("../Utils/errorHandler");
+const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("./catchAsyncError");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModels");
 
 exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
   const { token } = req.cookies;
-  // console.log("Token is ========:  :  " ,token);
+
   if (!token) {
-    return next(new ErrorHandler("Please login to access the data", 401));
+    return next(new ErrorHandler("Please Login to access this resource", 401));
   }
 
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
   req.user = await User.findById(decodedData.id);
 
   next();
 });
 
-exports.authorizedRoles = (...roles) => {
+exports.authorizeRoles = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-        return next(
-          new ErrorHandler(
-        `Role: ${req.user.role} is not allowed to access these resources`
-      , 403)
-     )
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resouce `,
+          403
+        )
+      );
     }
+
     next();
   };
 };
