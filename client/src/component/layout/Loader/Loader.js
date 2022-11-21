c21fda4f771d63bdaa04478f1f14e4ff27d24669
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-11-21 15:38:07 +0600

    Update Loader.js

--- client/src/component/layout/Loader/Loader.js
+++ client/src/component/layout/Loader/Loader.js
@@ -1,10 +1,31 @@
 import React from "react";
-import "./Loader.css";
+import "./module.Loader.css";
 
 const Loader = () => {
   return (
-    <div className="loading">
-      <div></div>
+    <div class="load-main">
+      <div class="load-container loading">
+        <div class=" loading-overlay"></div>
+        <div class="loading-anim">
+          <div class="border out"></div>
+          <div class="border in"></div>
+          <div class="border mid"></div>
+          <div class="circle">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
